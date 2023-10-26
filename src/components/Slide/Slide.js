a12069f1433379a6eb12750f366df3d5243b083a
Amila T Kumarasekara <84329334+amila-t-kumarasekara@users.noreply.github.com>
2023-10-26 13:54:16 +0530

    Implement the functionality for API calling and  implement the functionality for the Carousel Functions

---
+++ src/components/Slide/Slide.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Slide = ({ image, title, subtitle, index, currentIndex, transition }) => {
+  const active = index === currentIndex;
+
+  const slideStyle = {
+    transform: `translateX(${100 * (index - currentIndex)}%)`,
+    transition: transition === 'slide' ? 'transform 0.5s ease' : 'opacity 0.5s ease',
+  };
+
+  const imageStyle = {
+    position: 'relative',
+  };
+
+  const contentStyle = {
+    position: 'absolute',
+    bottom: '90px',
+    left: '0',
+    right: '0',
+    color: 'black',
+    padding: '10px',
+    textAlign: 'center',
+  };
+
+  return (
+    <div style={slideStyle} className={`slide ${active ? 'block' : 'hidden'}`}>
+      <div style={imageStyle}>
+        <img src={image} alt={title}  style={{ maxWidth: '100%', height: 'auto' }}/>
+        <div className='flex flex-col capitalize gap-3' style={contentStyle}>
+          <h2 className='font-bold text-[31px]'>{title}</h2>
+          <p className='text-[17px]'>{subtitle}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
