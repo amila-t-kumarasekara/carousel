18f379f318a8e4eb86eb860cbd5829df9425d8d7
Amila T Kumarasekara <84329334+amila-t-kumarasekara@users.noreply.github.com>
2023-10-26 14:25:20 +0530

    Create Component to navigate the default input props

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -8,7 +8,31 @@ export default function LandingPage() {
 
   return (
     <>
-    <Carousel slides={slides} infinite={infinite} />
+      <div className='bg-black'>
+        <div className='container mx-auto py-10'>
+          <div className='flex gap-4 items-center justify-center'>
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => {
+              setSlides(1);
+              setInfinite(false);
+              }}>
+              Slide 1
+            </button>
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => {
+              setSlides(4);
+              setInfinite(true);
+              }}>
+              Slide 4
+            </button>
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => {
+              setSlides(10);
+              setInfinite(false);
+              }}>
+              Slide 10
+            </button>
+          </div>
+        </div>
+      </div>
+      <Carousel slides={slides} infinite={infinite} />
     </>
   )
 }
